ddbd996fa661695794e384c750d141cd57310e34
Abdallah-Moh <abdallah.mam29@gmail.com>
2021-07-11 18:21:12 +0200

    Uncomment some code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 let osName = "your OS";
-// if (navigator.platform.indexOf("Win") != -1) {
-//   osName = "Windows";
-// } else if (navigator.platform.indexOf("Mac") != -1) {
-//   osName = "Mac";
-// } else if (navigator.platform.indexOf("Linux") != -1) {
-//   osName = "Linux";
-// }
+if (navigator.platform.indexOf("Win") != -1) {
+  osName = "Windows";
+} else if (navigator.platform.indexOf("Mac") != -1) {
+  osName = "Mac";
+} else if (navigator.platform.indexOf("Linux") != -1) {
+  osName = "Linux";
+}
 
 console.log(navigator);
 
